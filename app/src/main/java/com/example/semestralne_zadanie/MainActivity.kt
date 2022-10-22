161bc450c9b50e8d5a8c600f757256d57d458d5b
cybjorge <sambal258@gmail.com>
2022-10-22 12:05:51 +0200

    Z1 - https://android.mpage.sk/cvicenie1.php - Lottie animation

--- app/src/main/java/com/example/semestralne_zadanie/MainActivity.kt
+++ app/src/main/java/com/example/semestralne_zadanie/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val an=findViewById<com.airbnb.lottie.LottieAnimationView>(R.id.animation_view)
+
+        an.setOnClickListener {
+            an.playAnimation()
+        }
+
     }
 }
