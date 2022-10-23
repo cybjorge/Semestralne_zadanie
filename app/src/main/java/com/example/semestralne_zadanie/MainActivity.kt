8a81498402f7f79e15948305e622ad984570f534
cybjorge <sambal258@gmail.com>
2022-10-23 19:06:31 +0200

    Z2 - https://android.mpage.sk/cvicenie2.php - Refractoring + new fragments

--- app/src/main/java/com/example/semestralne_zadanie/MainActivity.kt
+++ app/src/main/java/com/example/semestralne_zadanie/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.semestralne_zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+    /*
         setContentView(R.layout.activity_main)
 
         val an=findViewById<com.airbnb.lottie.LottieAnimationView>(R.id.animation_view)
@@ -13,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         an.setOnClickListener {
             an.playAnimation()
         }
+        */
 
     }
 }
